960607b49e125c91504c3e09eebe047f9c0cae60
Markovic <130801866+K4nabis@users.noreply.github.com>
2024-08-14 00:40:51 +0200

    Add files via upload

---
+++ Zadatak 12/IMatrica.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_12
+{
+    internal interface IMatrica
+    {
+        double this[int i, int j] { get; }
+        int Dimenzija { get; }
+    }
+}
